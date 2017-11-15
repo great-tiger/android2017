f2f51bfcdbce9c3070fa8bdf0f11422f2cefb496
chenlihu <575569365@qq.com>
2017-11-15 17:40:19 +0800

    temp

---
+++ ReadContact/app/src/main/java/com/example/liqingliu/readcontact/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.liqingliu.readcontact;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
